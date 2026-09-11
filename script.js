4071eb42df97db7c191daac551964f863e5e1540
Kuro <alikaffe2009@gmail.com>
2026-09-11 10:37:54 +0200

    quiz

---
+++ script.js
@@ -0,0 +1,16 @@
+let questions = ["Hva er gpu?","Hva gjør windows L?","Er github og git det samme?","hva er cpu?","Hva er css brukt for?",]
+let answersleft = ["graphic card","låser pcen"]
+let answersright = ["general","Nei","styling for nettsiden."]
+const button1 = document.getElementById("button1")
+const button2 = document.getElementById("button2")
+const sporsmal = document.getElementById("sporsmal")
+let current = 0
+sporsmal.textContent = questions[current]
+button1.textContent = answersleft[current]
+button2.textContent = answersright[current]
+
+
+function answer1(){
+    current++
+    sporsmal.textContent = questions[current]
+}
